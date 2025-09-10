d9d91b8219f5195ea04f265db03ede293eac038d
RyanAmber <ryandiehl2027@gmail.com>
2025-09-10 11:39:21 +0000

    modified:   AI.java

--- AI.java
+++ AI.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class AI {
     public static void main(String[] args) {
-        int ans=3;
-        int[] amt=new int[6];
+        int ans=(int)(Math.random()*6+1);
+        System.out.println(ans);
+        int[] amt=new int[7];
         List<Integer> data=new ArrayList<Integer>();
         for (int i=0;i<18;i++){
             data.add(i%6+1);
-            amt[i%6]++;
+            amt[i%6+1]++;
         }
         for (int i=0;i<200;i++){
             int guess=data.get((int)(Math.random()*data.size()));
-            if (guess==ans&&amt[guess-1]<10){
+            if (guess==ans&&amt[guess]<10){
                 data.add(guess);
-            }else if (amt[guess-1]>1){
+                amt[guess]++;
+            }else if (amt[guess]>1){
                 data.remove(data.indexOf(guess));
+                amt[guess]--;
             }
-            //System.out.println(data);
             System.out.println(guess);
-            if (Math.random()<0.01&&ans<6){
-                ans++;
-                System.out.println("UP"+ans);
-            }else if(Math.random()<0.01&&ans>1){
-                ans--;
-                System.out.println("DOWN"+ans);
+            if (Math.random()<0.01){
+                ans=(int)(Math.random()*6+1);
+                //System.out.println("CHANGE"+ans);
+                //System.out.println(data);
             }
         }
     }
